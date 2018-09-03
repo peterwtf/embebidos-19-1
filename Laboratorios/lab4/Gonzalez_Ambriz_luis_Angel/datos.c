1dd164186e4ea315451289a2fdf89df41d951240
Luis Angel Gonzalez Ambriz <vivi_luis15@hotmail.com>
2018-09-03 14:29:58 -0500

    Agregando Archivos

---
+++ Laboratorios/lab4/Gonzalez_Ambriz_luis_Angel/datos.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#include "defs.h"
+
+int * reservarMemoria(){
+	int *mem;
+
+	mem = (int *) malloc(sizeof(int)*N);
+	if(!mem){
+		perror("Error al asignar memoria...\n");
+		exit(EXIT_FAILURE);
+	}
+
+	return mem;
+}
+
+
+void llenarArreglo(int *datos){
+	register int i;
+
+	srand(getpid()); //Semilla para generar random del número de proceso
+
+	for(i = 0; i < N; i++){
+		datos[i] = rand() % 255;
+	}
+}
+
+
+void imprimirArreglo(int *datos){
+	register int i;
+
+	for(i = 0; i < N; i++){
+
+		if(!(i%16))
+			printf("\n");
+
+		printf("%3d ", datos[i]);
+	}
+	printf("\n");
+}
---
+++ Laboratorios/lab4/Gonzalez_Ambriz_luis_Angel/procesamiento.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#include "defs.h"
+
+int mayorArreglo(int *datos){
+	register int i , mayor;
+
+	mayor = datos[0];
+
+	for(i = 1; i < N; i++){
+		if(datos[i]>mayor)
+			mayor = datos[i];
+	}
+
+	return mayor;
+}
+
+int menorArreglo(int *datos){
+	register int i , menor;
+
+	menor = datos[0];
+
+	for(i = 1; i < N; i++){
+		if(datos[i]<menor)
+			menor = datos[i];
+	}
+
+	return menor;
+}
+
+float promedioArreglo(int *datos){
+	register int i, suma;
+	float prom;
+
+	for(i = 0; i < N; i++){
+		suma = suma + datos[i];
+	}
+
+	prom = suma/N;
+
+	return prom;
+}
+
+void ordenaArreglo(int *datos)
+{
+	register int i, j, menor;
+	for (i = 0; i <N ; i++)
+	{
+		for (j = 0; j < N; j++)
+		{
+			if(!(datos[i]>datos[j]))
+			{
+				menor=datos[i];
+				datos[i]=datos[j];
+				datos[j]=menor;
+			}
+		}
+
+
+	}
+	//Método de Selección
+
+	
+}
+
+
