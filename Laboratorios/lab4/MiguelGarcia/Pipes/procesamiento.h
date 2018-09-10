27e50699e110f427a9666b7a34248e2ac8c6e817
Miguel GC <j_miguel_g_cebada@hotmail.com>
2018-09-10 02:11:41 -0500

    Agregando archivos al Laboratorio 4.2

---
+++ Laboratorios/lab4/MiguelGarcia/Pipes/procesamiento.h
@@ -0,0 +1,10 @@
+
+#ifndef PROC_H
+#define PROC_H
+
+int mayorArreglo( int *datos );
+int menorArreglo( int *datos );
+int promedioArreglo( int *datos );
+void ordenarArreglo( int *datos );
+
+#endif
