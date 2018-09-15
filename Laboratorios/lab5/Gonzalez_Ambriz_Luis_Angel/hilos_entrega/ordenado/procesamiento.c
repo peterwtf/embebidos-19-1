f6ee09f61bdd98a860fc659b940f559c497f4fe8
luis42 <vivi_luis15@hotmail.com>
2018-09-15 12:10:03 -0500

    Agregando Archivos

--- Laboratorios/lab5/Gonzalez_Ambriz_Luis_Angel/hilos_entrega/ordenado/procesamiento.c
+++ Laboratorios/lab5/Gonzalez_Ambriz_Luis_Angel/hilos_entrega/ordenado/procesamiento.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<sys/wait.h>
 
 #include "defs.h"
 
